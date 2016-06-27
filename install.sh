768565ec02c928dacecd74cdfb97357ef154ebc2
Michele Laurenti <laurenti.280@gmail.com>
2016-06-27 17:11:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,21 +6,22 @@ currdir=`pwd`
 for d in $install_dirs ; do
 	for f in `ls -1 $d` ; do
 		fname="${HOME}/.${f}"
-		echo "Installing file ${fname}..."
-		if [ -e ${fname} ] ; then
-			echo "mv $fname \"${fname}.bak\""
-			mv $fname "${fname}.bak"
-		elif [ -L ${fname} ] ; then
+		if [ -L ${fname} ] ; then
 			echo "Removing link ${fname}..."
 			rm $fname
+		elif [ -e ${fname} ] ; then
+			echo "mv $fname \"${fname}.bak\""
+			mv $fname "${fname}.bak"
+		else
+			echo "Installing file ${fname}..."
+			stripdir="$(echo "${d}" | tr -d '[[:space:]]')"
+			echo "ln -s ${currdir}/${stripdir}/${f} ${fname}"
+			ln -s ${currdir}/${stripdir}/${f} ${fname}
 		fi
-		stripdir="$(echo "${d}" | tr -d '[[:space:]]')"
-		echo "ln -s ${currdir}/${stripdir}/${f} ${fname}"
-		ln -s ${currdir}/${stripdir}/${f} ${fname}
 	done
 done
 
-if [ ! -e "~/.oh-my-zsh" ] ; then
+if [ ! -e "${HOME}/.oh-my-zsh" ] ; then
 	echo "Installing oh-my-zsh..."
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
@@ -28,13 +29,15 @@ fi
 powerlinedir="${currdir}/fonts"
 
 if [ ! -e ${powerlinedir} ] ; then
-	git clone https://github.com/powerline/fonts
-	cd fonts
+	git clone https://github.com/powerline/fonts ${powerlinedir}
+	cd ${powerlinedir}
 	./install.sh
-	cd ..
+	cd ${currdir}
 fi
 
-if [ ! -e ${currdir}/vim/vim/bundle/Vundle.vim ] ; then
-	git clone https://github.com/VundleVim/Vundle.vim.git vim/vim/bundle/Vundle.vim
+vundledir=${currdir}/vim/vim/bundle/Vundle.vim"
+
+if [ ! -e ${vundledir} ] ; then
+	git clone https://github.com/VundleVim/Vundle.vim.git ${vundledir}
 fi
 
