4e012d69644dc582093dbb53fbb009b4c6cd3fc1
Michele Laurenti <asmeikal@me.com>
2016-09-28 10:07:34 +0200

    Update zsh profiles.

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #/bin/sh
 
-install_dirs="vim bash zsh git"
 currdir=`pwd`
-
-./install-dir.sh $install_dirs
 
 if [ ! -e "${HOME}/.oh-my-zsh" ] ; then
 	echo "Installing oh-my-zsh..."
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	ln -s "${currdir}/themes" "${HOME}/.oh-my-zsh/custom/themes"
 fi
 
 powerlinedir="${currdir}/fonts"
@@ -24,4 +22,8 @@ vundledir="${currdir}/vim/vim/bundle/Vundle.vim"
 if [ ! -e ${vundledir} ] ; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ${vundledir}
 fi
+
+install_dirs="vim bash zsh git"
+
+./install-dir.sh $install_dirs
 
