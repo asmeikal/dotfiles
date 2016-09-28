681a5411cfdbd6828aa7daccebd645e17e490242
Michele Laurenti <asmeikal@me.com>
2016-09-28 10:17:44 +0200

    Remove old stuff, add pass git helper config

--- install.sh
+++ install.sh
@@ -2,14 +2,15 @@
 
 currdir=`pwd`
 
+# install oh-my-zsh
 if [ ! -e "${HOME}/.oh-my-zsh" ] ; then
 	echo "Installing oh-my-zsh..."
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	ln -s "${currdir}/themes" "${HOME}/.oh-my-zsh/custom/themes"
 fi
 
+# install powerline fonts
 powerlinedir="${currdir}/fonts"
-
 if [ ! -e ${powerlinedir} ] ; then
 	git clone https://github.com/powerline/fonts ${powerlinedir}
 	cd ${powerlinedir}
@@ -17,13 +18,16 @@ if [ ! -e ${powerlinedir} ] ; then
 	cd ${currdir}
 fi
 
+# install vundle
 vundledir="${currdir}/vim/vim/bundle/Vundle.vim"
-
 if [ ! -e ${vundledir} ] ; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ${vundledir}
 fi
 
-install_dirs="vim bash zsh git"
-
+# link everything
+install_dirs="vim zsh git"
 ./install-dir.sh $install_dirs
+
+# pass git helper
+ln -s "${currdir}/pass-git-helper" "${HOME}/.config/pass-git-helper"
 
