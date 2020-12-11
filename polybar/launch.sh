25cd805eee18b2708efaa8d04c3e48f0c173a445
Michelle Laurenti <asmeikal@me.com>
2020-12-11 15:33:36 +0100

    add some stuff

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top &
-polybar bottom &
+polybar top >>/tmp/polybar-top.log 2>&1 &
+polybar bottom >>/tmp/polybar-bottom.log 2>&1 &
 
 echo "Bars launched..."
